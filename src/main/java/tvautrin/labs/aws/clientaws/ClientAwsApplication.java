3920abe69440ae0e5b5fadd1a8e2958ac2b57625
Thomas VAUTRIN <tvautrin.work@gmail.com>
2017-11-28 17:05:52 +0100

    Initial Import

---
+++ src/main/java/tvautrin/labs/aws/clientaws/ClientAwsApplication.java
@@ -0,0 +1,12 @@
+package tvautrin.labs.aws.clientaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAwsApplication.class, args);
+	}
+}
